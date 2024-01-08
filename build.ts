859519f11eb6edeb3ea72c658eabaef7b37c708a
dong.huo <huodon@gmail.com>
2024-01-08 13:32:05 +0800

    WIP

--- build.ts
+++ build.ts
@@ -1,42 +1,44 @@
 /// <reference types="bun-types" />
 
 import { buildSync } from 'esbuild'
-import pkg from './package.json' assert { type: 'json' }
-import * as path from 'node:path'
 import * as fs from 'node:fs'
-import * as url from 'node:url'
+import * as path from 'node:path'
+import pkg from './package.json' assert { type: 'json' }
 
-const dirname = path.dirname(url.fileURLToPath(import.meta.url))
-const dist = path.join(dirname, 'dist')
+const isProduction = Bun.env.NODE_ENV === 'production'
+
+const dist = path.join(import.meta.dir, 'dist')
+
 if (fs.existsSync(dist)) {
-  if (Bun.env.NODE_ENV === 'production') process.exit(0)
+  if (isProduction) process.exit(0)
   console.log(`🧹 Cleaning dist folder...`)
   fs.rmSync(dist, { recursive: true, force: true })
 }
 
 const tsx = fs.readdirSync('./src/ui').filter((it) => it.endsWith('.tsx'))
 
-const exports = tsx.reduce(
-  (acc, it) => {
-    const entry = {
-      import: `./dist/ui/${it.replace('.tsx', '.js')}`,
-      types: `./src/ui/${it}`,
-    }
-    acc['./' + it.replace('.tsx', '')] = entry
+if (isProduction) {
+  const exports = tsx.reduce(
+    (acc, it) => {
+      const entry = {
+        import: `./dist/ui/${it.replace('.tsx', '.js')}`,
+        types: `./src/ui/${it}`,
+      }
+      acc['./' + it.replace('.tsx', '')] = entry
 
-    return acc
-  },
-  {} as Record<string, any>,
-)
-exports['./create-style-context'] = {
-  import: './dist/lib/create-style-context.js',
-  types: './src/lib/create-style-context.tsx',
+      return acc
+    },
+    {} as Record<string, any>,
+  )
+  exports['./create-style-context'] = {
+    import: './dist/lib/create-style-context.js',
+    types: './src/lib/create-style-context.tsx',
+  }
+
+  // @ts-ignore
+  pkg['exports'] = exports
+  fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2))
 }
-
-// @ts-ignore
-pkg['exports'] = exports
-
-fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2))
 
 buildSync({
   entryPoints: [
